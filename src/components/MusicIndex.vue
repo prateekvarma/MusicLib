e0b98e9e0949b8820eb96816676b8dee297f4a59
Prateek Varma <prtk.varma@gmail.com>
2021-04-06 14:23:33 -0600

    created MusicIndex component, created music route and display data from json

---
+++ src/components/MusicIndex.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Artist</th>
+            <th scope="col">Genre</th>
+            <th scope="col">Album</th>
+            </tr>
+        </thead>
+        <tbody v-for="(song, index) in songs" v-bind:key="index">
+            <tr>
+            <td>{{song.title}}</td>
+            <td>{{song.artist}}</td>
+            <td>{{song.genre}}</td>
+            <td>{{song.album}}</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+import MusicData from '../assets/list.json'
+
+export default {
+    data() {
+        return {
+            songs: MusicData
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
